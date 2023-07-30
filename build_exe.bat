a6737b00e10e5533fc8eccce7cc4e57d6d76279a
evvopilova <vopilova.elena@gmail.com>
2023-07-31 00:31:01 +0700

    add comments with environment variables for lib paths

--- build_exe.bat
+++ build_exe.bat
@@ -1,2 +1,6 @@
+:: для компиляции необходимо прописать переменные среды
+:: NATASHA_DATA_PATH = C:\Users\User\AppData\Local\Programs\Python\Python311\Lib\site-packages\natasha\data
+:: PYMORPHY2_DICT_PATH = C:\Users\User\AppData\Local\Programs\Python\Python311\Lib\site-packages\pymorphy2_dicts_ru\data
+
 C:\Users\User\AppData\Local\Programs\Python\Python311\Scripts\pyinstaller -F main.py
 pause
--- build_exe.py
+++ build_exe.py
@@ -1,3 +1,7 @@
+# для компиляции необходимо прописать переменные среды
+# NATASHA_DATA_PATH = C:\Users\User\AppData\Local\Programs\Python\Python311\Lib\site-packages\natasha\data
+# PYMORPHY2_DICT_PATH = C:\Users\User\AppData\Local\Programs\Python\Python311\Lib\site-packages\pymorphy2_dicts_ru\data
+
 import PyInstaller.__main__
 
 PyInstaller.__main__.run(['main.py', '--onefile'])
